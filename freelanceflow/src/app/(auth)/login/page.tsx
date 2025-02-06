5f6f98130f657ba87e97ce83407396559efa93d4
Hamid-dev13 <hamid.bennacef@epitech.eu>
2025-02-06 17:10:56 +0100

    oui

--- freelanceflow/src/app/(auth)/login/page.tsx
+++ freelanceflow/src/app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { error } from "console";
+
 
 export default function LoginPage() {
     const [email, setEmail] = useState("");
